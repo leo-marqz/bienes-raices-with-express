424baa8a065cea62dfbc26681605e5e4cb6cd1a1
Elmer Marquez <leomarqz2020@gmail.com>
2024-12-22 22:28:59 -0600

    Se esta creando una api con la cual se cargaran datos en la vista publica principal

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,6 +1,21 @@
 
+import { Property, Category, Price } from "../models/index.js";
+
 async function getProperties(req, res) {
-    res.json({ message: "GET properties" });
+    const properties = await Property.findAll({
+        include: [
+            {
+                model: Category,
+                as: "category"
+            },
+            {
+                model: Price,
+                as: "price"
+            }
+        ]
+    });
+
+    res.json({ properties });
 }
 
 async function getCategories(req, res) {
